38183799b060eb6828a1c8e695d5b5abb6f8de58
bihius <k.kasperkiewicz@proton.me>
2024-05-23 00:42:24 +0200

    start of collab, changes unknown

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# COPYRIGHT: bihius.me
+# AUTHOR: bihius
+# TIME SPENT: 6 hours
+
+# This file is the main file that runs the website. It imports the create_app function from the website package and runs the app.
+
 from website import create_app
 
 app = create_app()
